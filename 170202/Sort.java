7b129fc15f525a7593109d5bb6aeb61fdfb3fc07
Mateusz Milcarz <k1877052@mvrht.com>
2017-02-27 08:31:02 +0000

    Sort

--- 170202/Sort.java
+++ 170202/Sort.java
@@ -1,5 +1,23 @@
 import java.util.Random;
-public class Losowanie{
-    public static void main(String[] args){
+import java.util.Arrays;
+public class Sort{
+ int[] losuj(){
+    int [] T= new int[10];
+    Random rand= new Random();
+    for(int i=0; i<10;i++){
+        T[i]= rand.nextInt(100-0+1)+0;
     }
-}s
+    return T;
+}
+
+    void wypisz (int[] T){
+        Arrays.sort(T);
+        for(int j=0;j<T.length;j++){
+            System.out.println(T[j]+" ,");
+            
+        }
+    }
+    public static void main(String[] args){
+        Sort tab=new Sort();
+        tab.wypisz(tab.losuj());
+}}
